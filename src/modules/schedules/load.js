71689ed38a7d4336b6912eff9763b0e597beffc6
Giovanna Leme Amadeu <80579735+giigio@users.noreply.github.com>
2025-04-01 14:14:41 -0300

    feat(schedules): implementa busca e exibição de agenda diária

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,9 +1,14 @@
-import { hoursLoad } from "../form/hours-load";
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js";
+import { hoursLoad } from "../form/hours-load.js";
+import { schedulesShow } from "./show.js";
 
 const selectedDate = document.getElementById("date");
 
-export function schedulesDay() {
+export async function schedulesDay() {
   const date = selectedDate.value;
+
+  const dailySchedules = await scheduleFetchByDay({ date });
+  schedulesShow({ dailySchedules });
 
   hoursLoad({ date });
 }
